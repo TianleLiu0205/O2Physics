d633b5dbaced15e7c801a504a2cd7ba44206ed63
TianleLiu0205 <liutl1121@mails.jlu.edu.cn>
2026-08-25 01:12:37 +0800

    remove TPCnCRover

--- PWGDQ/Core/CutsLibrary.cxx
+++ PWGDQ/Core/CutsLibrary.cxx
@@ -930,7 +930,7 @@ if (!nameStr.compare("kaonPt0to6_noPID")) {
   qualityCuts->AddCut(VarManager::kTPCchi2, 0.0, 4.0);
   qualityCuts->AddCut(VarManager::kITSchi2, 0.0, 36.0);
   qualityCuts->AddCut(VarManager::kTPCncls, 70.0, 999.0);
-  qualityCuts->AddCut(VarManager::kTPCnCRoverFindCls, 0.8, 2.0);
+  //qualityCuts->AddCut(VarManager::kTPCnCRoverFindCls, 0.8, 2.0);
   // qualityCuts->AddCut(VarManager::kPVContributor, 0.5, 1.5);
   cut->AddCut(kineCut);
   cut->AddCut(qualityCuts);
@@ -947,7 +947,7 @@ if (!nameStr.compare("kaonPt0to6")) {
   qualityCuts->AddCut(VarManager::kTPCchi2, 0.0, 4.0);
   qualityCuts->AddCut(VarManager::kITSchi2, 0.0, 36.0);
   qualityCuts->AddCut(VarManager::kTPCncls, 70.0, 999.0);
-  qualityCuts->AddCut(VarManager::kTPCnCRoverFindCls, 0.8, 2.0);
+  //qualityCuts->AddCut(VarManager::kTPCnCRoverFindCls, 0.8, 2.0);
   // qualityCuts->AddCut(VarManager::kPVContributor, 0.5, 1.5);
   cut->AddCut(kineCut);
   cut->AddCut(qualityCuts);
